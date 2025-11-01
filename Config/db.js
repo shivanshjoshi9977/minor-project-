e16926ae7bd114748ff52e6fcf7ab9c4de49f606
Ayushkothari47 <ayushkotharii47@gmail.com>
2025-11-01 14:06:41 +0530

    update

--- Config/db.js
+++ Config/db.js
@@ -2,11 +2,12 @@ const mongoose =  require("mongoose");
 const db = async () => {
     try{
         await mongoose.connect("mongodb+srv://shivanshjoshi9977:12345@cluster0.h6slwuv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
+        console.log("MongoDB connected");
     }
     catch(error){
         console.log(error.message); 
         process.exit(1)
     }
 }
-console.log("MongoDB connected");
 module.exports = db;
